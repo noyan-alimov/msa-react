462c91287a41437e7264129ab921d06ec141f4f8
Noyan <noyan.alimov@gmail.com>
2021-04-23 19:03:35 +0100

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: [
+		'./src/**/*.tsx',
+		'./src/**/*.ts',
+		'./src/**/*.jsx',
+		'./src/**/*.js',
+		'./index.html',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
